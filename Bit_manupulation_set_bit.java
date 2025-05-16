c69f262d4b2a6d68bd8a1bc6152a83e35659dc68
Ritesh9322 <145695029+Ritesh9322@users.noreply.github.com>
2025-05-16 12:51:24 +0530

    Add files via upload

---
+++ Bit_manupulation_set_bit.java
@@ -0,0 +1,21 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.util.WeakHashMap;
+
+public class Bit_manupulation_set_bit
+{
+    public static void main(String[] args)
+    {
+        int num=10111;
+        int i=2;
+        while (i>=0){
+            num=num/10;
+            i--;
+
+        }
+        int rem=num%10;
+        
+
+
+    }
+}
